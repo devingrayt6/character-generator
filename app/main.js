349b8fb9489d2ff3229c9ce305c04c6e0c305303
Riley Tudbury <tudbury@gmail.com>
2020-02-06 15:22:59 -0700

    linked charactercontroller to main

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 import ValuesController from "./Controllers/ValuesController.js";
+import CharacterController from "./Controllers/CharacterController.js"
 
 class App {
   valuesController = new ValuesController();
+  characterController = new CharacterController();
 }
 
 window["app"] = new App();
