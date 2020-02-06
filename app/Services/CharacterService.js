a65307466e6e1e0c4a06376ad41393a7460141ed
Riley Tudbury <tudbury@gmail.com>
2020-02-06 16:04:39 -0700

    Haha I can commit

--- app/Services/CharacterService.js
+++ app/Services/CharacterService.js
@@ -12,32 +12,15 @@ class CharacterService {
       var rollArray = []
       rollArray.push(roll)
     }
-
     removeSmallest(rollArray)
-
-    let rollSum
-
+    let rollSum = 0
     for (let i = 0; i < rollArray.length; i++) {
       rollSum += rollArray[i]
     }
-    // let first =rollArray[0];
-    // let location;
-    // for(let i=0;i<rollArray.length;i++){
-    //   if(i+1<rollArray.length){
-    //   if(first<=rollArray[i+1]){
-    //     location =i;
-    //   }
-    //   else{
-    //     first=rollArray[i+1]
-    //   }
-    // }
-    // else{
-    //   rollArray[location]
-    // }
+    return rollSum
   }
 }
 
-}
 
 
 const CHARACTERSERVICE = new CharacterService()
--- app/utils.js
+++ app/utils.js
@@ -1,4 +1,5 @@
 function removeSmallest(array) {
   let min = Math.min(...array);
+  console.log(min)
   return array.filter(e => e != min);
 }
