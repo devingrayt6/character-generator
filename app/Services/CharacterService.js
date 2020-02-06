d1d35b74dcfc4e9bec110ac4896a1232a8466b17
Riley Tudbury <tudbury@gmail.com>
2020-02-06 15:55:53 -0700

    Opened utils

--- app/Services/CharacterService.js
+++ app/Services/CharacterService.js
@@ -9,13 +9,36 @@ class CharacterService {
 
     for (let i = 0; i < 4; i++) {
       let roll = Math.floor((Math.random() * 6) + 1)
-      console.log(roll)
+      var rollArray = []
+      rollArray.push(roll)
     }
 
+    removeSmallest(rollArray)
 
+    let rollSum
+
+    for (let i = 0; i < rollArray.length; i++) {
+      rollSum += rollArray[i]
+    }
+    // let first =rollArray[0];
+    // let location;
+    // for(let i=0;i<rollArray.length;i++){
+    //   if(i+1<rollArray.length){
+    //   if(first<=rollArray[i+1]){
+    //     location =i;
+    //   }
+    //   else{
+    //     first=rollArray[i+1]
+    //   }
+    // }
+    // else{
+    //   rollArray[location]
+    // }
   }
+}
 
 }
+
 
 const CHARACTERSERVICE = new CharacterService()
 
