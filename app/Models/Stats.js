8256948be09a63a26d95b3e3406d54785cf3d857
brianwiggins <peppermint.butl3r@gmail.com>
2020-02-06 15:50:59 -0700

    finished stats class and getStatmod method

--- app/Models/Stats.js
+++ app/Models/Stats.js
@@ -1,39 +1,86 @@
 export class Stats{
   constructor(data){
-    this.wisdom=data.wisdom;
-    this.intelligence=data.wisdom;
-    this.strength=data.strength;
-    this.constitution=data.constitution;
-    this.dex=data.dex;
-    this.charisma=data.charisma;
-  }
-  getWisMod(){
-    let mod;
-    return mod;
+    this.wisScore=data.wisdomScore;
+    this.intScore=data.intScore;
+    this.strScore=data.strScore;
+    this.constScore=data.constScore;
+    this.dexScore=data.dexScore;
+    this.charScore=data.charScore;
   }
 
-  getIntMod(){
-    let mod;
-    return mod;
-  }
+  //gets modifier depending on which attribute needs to be checked
+  //IMPORTANT:
+  //passed in value must be string, ONE of following values:"str","int","wis","const","dex","char"
+  getStatMod(attribute){
 
-  getStrMod(){
     let mod;
-    return mod;
-  }
 
-  getConstMod(){
-    let mod;
-    return mod;
-  }
+    switch(attribute){
 
-  getDexMod(){
-    let mod;
-    return mod;
-  }
+      case "str":
+        mod = modCalc(this.strScore);
+        break;
 
-  getCharMod(){
-    let mod;
+      case "int":
+        mod= modCalc(this.intScore);
+        break;
+
+      case "wis":
+        mod= modCalc(this.wisScore);
+        break;
+
+      case "const":
+        mod= modCalc(this.constScore);
+        break;
+
+      case "dex":
+        mod= modCalc(this.dexScore);
+        break;
+
+      case "char":
+        mod= modCalc(this.charScore);
+        break;
+      
+      
+    }
     return mod;
   }
+  
+  }
+  //calculates the modifier score based on attribute value
+  function modCalc(score){
+    let mod;
+    //determine attribute score
+    if(score<2){
+      mod=-5;
+    }else if(score<=3){
+      mod=-4;
+    }else if(score<=5){
+      mod=-3
+    }else if(score<=7){
+      mod=-2
+    }else if(score<=9){
+      mod=-1
+    }else if(score<=11){
+      mod=0
+    }else if(score<=13){
+      mod=1
+    }else if(score<=15){
+      mod=2
+    }else if(score<=17){
+      mod=3
+    }else if(score<=19){
+      mod=4
+    }else if(score<=21){
+      mod=5
+    }else if(score<=23){
+      mod=6
+    }else if(score<=25){
+      mod=8
+    }else if(score<=27){
+      mod=9
+    }else if(score<=30){
+      mod=10
+    }
+    return mod;//return modifier value
   }
